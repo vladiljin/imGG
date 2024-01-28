3b031971513caf577ccc278a3f3d831185480daa
vladimir iljin <157532871+vladiljin@users.noreply.github.com>
2024-01-28 15:42:23 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.relativelayout import RelativeLayout
+
+class BoxLayoutMain(BoxLayout):
+    def __init__(self, **kwargs):
+        super(BoxLayoutMain, self).__init__(**kwargs)
+        # Construct the path to the image file
+        self.image_path = os.path.join("data", "image.png")
+
+
+
+class OverlayLayout(RelativeLayout):
+    pass
+
+class MyApp(App):
+    def build(self):
+        return BoxLayoutMain()
+
+if __name__ == "__main__":
+    MyApp().run()
